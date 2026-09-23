efbc0d5a5132d2f586d579b27662f7a14e24222e
rafaelmaxfs <rafaelmaxfgs@gmail.com>
2026-09-23 17:11:20 -0300

    Excluir script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const customCursor = document.getElementById('custom-cursor');
-
-document.addEventListener('mousemove', (e) => {
-    customCursor.style.left = `${e.clientX}px`;
-    customCursor.style.top = `${e.clientY}px`;
-});
